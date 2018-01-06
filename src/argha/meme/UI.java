e40b68d616e6452f8d3c97b4d51dcef101b7166f
Argha Das <shadestreak@users.noreply.github.com>
2018-01-07 02:52:30 +0530

    released v1.0

--- src/argha/meme/UI.java
+++ src/argha/meme/UI.java
@@ -17,10 +17,16 @@
 package argha.meme;
 
 import argha.meme.Utils.ImageUtil;
+import javax.swing.JOptionPane;
 
 public class UI extends javax.swing.JFrame {
 
+    private ImageUtil imageUtil;
+    private Save save;
+
     public UI() {
+        imageUtil = new ImageUtil();
+        save = new Save();
         initComponents();
     }
 
@@ -28,28 +34,47 @@ public class UI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        designer1 = new argha.meme.generatorCompnent.Designer();
+        designer = new argha.meme.generatorCompnent.Designer();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem10 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
+        jMenuItem8 = new javax.swing.JMenuItem();
+        jMenuItem9 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        jMenuItem7 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Meme Maker v1.0");
         setResizable(false);
 
-        javax.swing.GroupLayout designer1Layout = new javax.swing.GroupLayout(designer1);
-        designer1.setLayout(designer1Layout);
-        designer1Layout.setHorizontalGroup(
-            designer1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout designerLayout = new javax.swing.GroupLayout(designer);
+        designer.setLayout(designerLayout);
+        designerLayout.setHorizontalGroup(
+            designerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 500, Short.MAX_VALUE)
         );
-        designer1Layout.setVerticalGroup(
-            designer1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        designerLayout.setVerticalGroup(
+            designerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 500, Short.MAX_VALUE)
         );
 
         jMenu1.setText("File");
 
-        jMenuItem1.setText("Pick Image");
+        jMenuItem2.setText("Pick Image");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem2);
+
+        jMenuItem1.setText("Save Image");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem1ActionPerformed(evt);
@@ -57,7 +82,42 @@ public class UI extends javax.swing.JFrame {
         });
         jMenu1.add(jMenuItem1);
 
+        jMenuItem10.setText("Exit");
+        jMenuItem10.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem10ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem10);
+
         jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Tools");
+
+        jMenu4.setText("Helper");
+
+        jMenuItem8.setText("Enable");
+        jMenu4.add(jMenuItem8);
+
+        jMenuItem9.setText("Disable");
+        jMenu4.add(jMenuItem9);
+
+        jMenu2.add(jMenu4);
+
+        jMenuItem5.setText("Propertion");
+        jMenu2.add(jMenuItem5);
+
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Help");
+
+        jMenuItem6.setText("Update");
+        jMenu3.add(jMenuItem6);
+
+        jMenuItem7.setText("About");
+        jMenu3.add(jMenuItem7);
+
+        jMenuBar1.add(jMenu3);
 
         setJMenuBar(jMenuBar1);
 
@@ -65,20 +125,40 @@ public class UI extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(designer1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(designer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(designer1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(designer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        designer.storeImage(imageUtil.scaleIt(designer.getWidth(), designer.getHeight()));
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-        ImageUtil util = new ImageUtil();
-        designer1.disableHelpers();
+        designer.disableHelpers();
+        String fileName = JOptionPane.showInputDialog("Enter File Name");
+        save.setFileName(fileName);
+        save.setComponent(designer);
+        save.saveComponent();
+        System.out.println("Image Saved");
+        designer.enableHelper();
     }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jMenuItem10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem10ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jMenuItem10ActionPerformed
 
     public static void main(String args[]) {
         try {
@@ -101,9 +181,19 @@ public class UI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private argha.meme.generatorCompnent.Designer designer1;
+    private argha.meme.generatorCompnent.Designer designer;
     private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem10;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
+    private javax.swing.JMenuItem jMenuItem9;
     // End of variables declaration//GEN-END:variables
 }
--- src/argha/meme/generatorCompnent/Designer.java
+++ src/argha/meme/generatorCompnent/Designer.java
@@ -37,25 +37,27 @@ public class Designer extends JPanel {
     //Top Variables
     private Rectangle top = new Rectangle(10, 10, 50, 50);
     private Color topColor = Color.WHITE;
+    private Color topTextColor = Color.WHITE;
     private int topTextSize = 50;
     private String topText = "TOP";
 
     //Bottom Variables
     private Rectangle bottom = new Rectangle(100, 10, 50, 50);
     private Color bottomColor = Color.WHITE;
+    private Color bottomTextColor = Color.WHITE;
     private int bottomTextSize = 50;
     private String bottomText = "BOTTOM";
 
     //Helper properties
     int topWidth, topHeight;
     int bottomWidth, bottomHeight;
-
+    
     public Designer() {
         designerUtil = new DesignerUtil(this);
         designerUtil.registerTop(top);
         designerUtil.registerBottom(bottom);
     }
-
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -69,6 +71,7 @@ public class Designer extends JPanel {
         //Paint Top
         graphics.setPaint(topColor);
         graphics.draw(top);
+        graphics.setPaint(topTextColor);
         Font topFont = new Font("Tahoma", Font.BOLD, topTextSize);
         graphics.setFont(topFont);
         graphics.drawString(topText, designerUtil.getRectTopX(), designerUtil.getRectTopY());
@@ -76,25 +79,26 @@ public class Designer extends JPanel {
         //Paint Bottom
         graphics.setPaint(bottomColor);
         graphics.draw(bottom);
+        graphics.setPaint(bottomTextColor);
         Font bottomFont = new Font("Tahoma", Font.BOLD, bottomTextSize);
         graphics.setFont(bottomFont);
         graphics.drawString(bottomText, designerUtil.getRectBottomX(), designerUtil.getRectBottomY());
     }
-
+    
     public void storeImage(Image img) {
         this.image = img;
         repaint();
     }
-
+    
     public void enableHelper() {
         top.height = topHeight;
         top.width = topWidth;
         bottom.height = bottomHeight;
         bottom.width = bottomWidth;
-
+        
         repaint();
     }
-
+    
     public void disableHelpers() {
         topWidth = top.width;
         topHeight = top.height;
@@ -106,23 +110,36 @@ public class Designer extends JPanel {
         bottom.width = 0;
         repaint();
     }
-
+    
     public void changeTopSize(int size) {
         this.topTextSize = size;
         repaint();
-
+        
     }
-
+    
     public void changeTopText(String text) {
         this.topText = text;
         repaint();
     }
-
-    public void changeTopTextColor() {
+    
+    public void changeTopTextColor(Color c) {
+        this.topTextColor = c;
         repaint();
     }
-
+    
     public void changeBottomSize(int size) {
-        
+        bottomTextSize = size;
+        repaint();
     }
+    
+    public void changeBottomText(String text) {
+        this.bottomText = text;
+        repaint();
+    }
+    
+    public void changeBottomTextColor(Color c) {
+        this.bottomTextColor = c;
+        repaint();
+    }
+    
 }
